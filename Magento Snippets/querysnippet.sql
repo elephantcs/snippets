8943cf1811851d173b510755f8c51e716baed725
Freek Bron <fsbron@gmail.com>
2015-01-30 12:20:42 +0100

    improved magento querysnippet and added comment for mamp users

--- Magento Snippets/querysnippet.sql
+++ Magento Snippets/querysnippet.sql
@@ -1,10 +1,11 @@
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/unsecure/base_url’;
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/secure/base_url’;
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/unsecure/base_link_url’;
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/unsecure/base_skin_url’;
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/unsecure/base_media_url’;
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/unsecure/base_js_url’;
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/secure/base_link_url’;
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/secure/base_skin_url’;
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/secure/base_media_url’;
-UPDATE core_config_data SET value="unsecure.domain.com" WHERE path=’web/secure/base_js_url’;
+UPDATE core_config_data SET value = REPLACE (value, 'http://staging.aladdin-express.nl/', 'http://freek.local:8888/aladdin/');
+
+/* MAMP users:
+ * 
+ * Table: core_config_data
+ * 	- scope: 'websites' 	path: 'web/secure/base_url'
+ * 	- scope: 'websites' 	path: 'web/unsecure/base_url'
+ * 
+ * Append 'index.php' to the value
+ * 
+ */
