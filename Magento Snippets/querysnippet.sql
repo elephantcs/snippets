bdbfce9e26f0ba82bb7deb49e42b86dec267b934
Martijn <martijn.schipper94@gmail.com>
2015-02-05 12:36:56 +0100

    Update querysnippet.sql

--- Magento Snippets/querysnippet.sql
+++ Magento Snippets/querysnippet.sql
@@ -1,4 +1,4 @@
-UPDATE core_config_data SET value = REPLACE (value, 'http://staging.aladdin-express.nl/', 'http://freek.local:8888/aladdin/');
+UPDATE core_config_data SET value = REPLACE (value, 'new_site', 'old_site');
 
 /* MAMP users:
  * 
